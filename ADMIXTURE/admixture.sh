9735c9902dd174bda56972c660123ba7c0078464
Laura Bertola <39679904+LauraBertola@users.noreply.github.com>
2023-09-15 11:39:39 +0200

    Update admixture.sh

--- ADMIXTURE/admixture.sh
+++ ADMIXTURE/admixture.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
-FILE=$1 # Path to bed file
-K=$2 # Number of ancestral populations
-N=$3 # Number of runs (different seeds)
-OUT=$4 # Output prefix
-THREADS=$5 # Number of threads to use
-TMP=`basename $FILE`
-DEF=${TMP::-4}
+file=$1 #plink filename without .bed
+num=$2  #number of iterations
+P=$3 #number of threads
+out=$4 #output directory (no /)
+K=$5 #number of populations
+bfile=`basename $file`
+echo num = $num
+echo p = $P
+echo out = $out
+echo K = $K
+if [ -f $file.bed ]
+then
+   echo file = $file
+else
+   echo File $file.bed does not exist.
+fi
 
-for i in `seq 1 $N`
-do
-        admixture $FILE $K -j$THREADS -s $i > $OUT.K$K.s$i.log
-        mv $DEF.$K.Q $OUT.K$K.s$i.Q
-        mv $DEF.$K.P $OUT.K$K.s$i.P
-        gzip $OUT.K$K.s$i.P
+touch $out/$bfile.$K.likes.tmp
+rm $out/$bfile.$K.likes.tmp
+
+for f in `seq 1 $num`
+        do
+    echo -n -e $f"\t" >> $out/$bfile.$K.likes.tmp
+    admixture $file.bed $K -s $f -j$P > $out/$bfile.$K.log_$f
+    mv $bfile.$K.Q $out/$bfile.$K.Q_$f
+    mv $bfile.$K.P $out/$bfile.$K.P_$f
+    grep ^Loglikelihood $out/$bfile.$K.log_$f | cut -f2 -d" " >> $out/$bfile.$K.likes.tmp
+    CONV=`Rscript -e "r<-read.table('$out/$bfile.$K.likes.tmp');r<-r[order(-r[,2]),];cat(sum(r[1,2]-r[,2]<5),'\n')"`
+
+    if [ $CONV -gt 2 ]
+    then
+        cp $out/$bfile.$K.Q_$f $out/$bfile.$K.Q_conv
+        cp $out/$bfile.$K.P_$f $out/$bfile.$K.P_conv
+        cp $out/$bfile.$K.log_$f $out/$bfile.$K.log_conv
+        break
+        fi
 done
+cat $out/$bfile.$K.likes.tmp | sort -k2 -n -r > $out/$bfile.$K.likes
