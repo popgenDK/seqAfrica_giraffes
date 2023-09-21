0574ca41cc26d3ffb905189be9e4b5e6faa30c1a
Laura Bertola <39679904+LauraBertola@users.noreply.github.com>
2023-09-21 13:59:49 +0200

    Update admixture.sh

--- ADMIXTURE/admixture.sh
+++ ADMIXTURE/admixture.sh
@@ -1,34 +1,24 @@
 #!/bin/bash
+file=$1 # PLINK prefix filename
+num=$2  # Number of ADMIXTURE runs per K
+threads=$3 # Number of threads
+out=$4 # Output directory
+K=$5 # Number of ancestral populations
+base=$(basename $file)
 
-file=$1 #input beagle file
-nfile=$2 #file name 
-num=$3  #maximum number of iterations
-P=$4 #number of threads/cores used
-K=$5 #number of populations
-NGSA=$6 - #NGSadmix path
+# File to store loglikelihoods
+touch ${out}/${base}.K$K.loglikes
 
-out=$out/$K #output directory
-
-mkdir -p $out
-
-for f in `seq $num`
+# Run ADMIXTURE for num times
+for s in `seq 1 $num`
 do
-    echo -n -e $f"\t"
-    echo $file
-    echo $K
-    #Run NGSadmix
-    $NGSA -minMaf 0.05 -likes $file -seed $f -K $K -P $P -printInfo 1 -o $out/$nfile.$K.$f
-    grep "like=" $out/$nfile.$K.$f.log | cut -f2 -d " " | cut -f2 -d "=" >> $out/$nfile.$K.likes
-    #Convergence criteria - #r = 10* no. of millions of SNPs
-    CONV=`Rscript -e "r<-read.table('$out/$nfile.$K.likes');r<-r[order(-r[,1]),];cat(sum(r[1]-r<148),'\n')"` 
-
-    #Check convergence
-    if [ $CONV -gt 2 ]  # 3 files meeting convergence criteria
-    then
-	cp $out/$nfile.$K.$f.qopt $out/$nfile.$K.$f.qopt_conv
-	cp $out/$nfile.$K.$f.fopt.gz $out/$nfile.$K.$f.fopt_conv.gz
-	cp $out/$nfile.$K.$f.log $out/$nfile.$K.$f.log_conv
-	break
-    fi
-
+		admixture ${file}.bed $K -s $s -j${threads} > ${out}/${base}.K${K}.s${s}.log
+		mv ${base}.${K}.Q ${out}/${base}.K${K}.s${s}.Q
+		mv ${base}.${K}.P ${out}/${base}.K${K}.s${s}.P
+		grep ^Loglikelihood ${out}/${base}.K${K}.s${s}.log | cut -f2 -d" " >> ${out}/${base}.K$K.loglikes
 done
+
+# Find best solution
+best=$(nl ${out}/${base}.K$K.loglikes | sort -k2 -n -r | head -n 1 | cut -f1 | tr -d ' ')
+cp ${out}/${base}.K${K}.s${best}.Q ${out}/${base}.K${K}.s${best}.best.Q
+cp ${out}/${base}.K${K}.s${best}.P ${out}/${base}.K${K}.s${best}.best.P
